aa37c1c0cb7add7200d1e593ba1c1d440ca9e5f7
mromadisiregar <mromadisiregar@gmail.com>
2024-02-15 20:42:05 +0700

    kembalikan ke normal

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ app.use('*', (req, res) => {
   res.json({ msg: 'GEDANG GORENG TEST' }).end()
 })
 
-const port = 4000
+const port = process.env.PORT || 3000
 app.listen(port, () => {
   console.log(`index.js listening on ${port}`)
 })
