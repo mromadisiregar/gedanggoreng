46797905e8aa0f579351e52fd6dcb07afcd6058b
mromadisiregar <mromadisiregar@gmail.com>
2024-04-08 22:44:07 +0700

    test telegram

--- README.md
+++ README.md
@@ -1,16 +1,1 @@
-TOKEN BOT PERNIKAHAN.APK
-
-Jadi, diketahui APK pernikahan yang terkenal itu mengirim semua data SMS ke dalam channel telegram.
-
-TOKEN DAN CHAT ID DITEMUKAN DI DALAM APK
-
-Repo ini digunakan untuk membuat bot kehabisan quota.
-
-```
-data: {
-  ok: false,
-  error_code: 429,
-  description: 'Too Many Requests: retry after 16',
-  parameters: [Object]
-}
-```
+TEST TEST SAJA
--- index.js
+++ index.js
@@ -1,70 +1,41 @@
-var express = require("express")
-var app = express()
-var bodyParser = require("body-parser")
-const axios = require("axios")
-const fs = require('fs');
-const textFilePath = 'file.txt';
-const telegramToken = '6873766343:AAErkuk9g_ea-IWXWYSXskVGVINDjR5iqhM';
-const chatId = '6705390884';
+const express = require('express');
+const bodyParser = require('body-parser');
+const TelegramBot = require('node-telegram-bot-api');
 
-// test push
+const token = '${{shared.TOKEN}}';
+const bot = new TelegramBot(token, { polling: true });
+const chatId = '${{shared.CHATID}}';
 
-const sendMessage = async (message) => {
-  axios
-		.post(
-			`https://api.telegram.org/bot${telegramToken}/sendMessage`,
-			{
-				chat_id: chatId,
-				text: message,
-			}
-		)
-		.then((response) => {
-			console.log("Message posted")
-		})
-		.catch((err) => {
-			console.log("Error :", err)
-		})
-};
+const app = express();
+const router = express.Router();
 
-const readAndSendMessages = async () => {
+app.use(bodyParser.urlencoded({ extended: true }));
+
+router.post('/bot_notifikasi_cpanel', async (req, res) => {
   try {
-    const content = fs.readFileSync(textFilePath, 'utf-8');
-    const sentences = content.split('.'); // You may need to adjust this based on your file format
-    try {
-      for (const sentence of sentences) {
-        if (sentence.trim() !== '') {
-          await sendMessage(sentence.trim());
-          // console.log(sentence.trim());
-          await new Promise(resolve => setTimeout(resolve, 1000)); // Wait for 1 second
-        }
-      }
-      console.log("Selesai kotbah");
-    } catch (error) {
-      console.error('Gagal mengirim pesan:', error);
+    const formData = req.body;
+    const receivedKey = formData.validation_key;
+    const validationKey = '${{shared.VALIDATIONKEY}}';
+    if (receivedKey !== validationKey) {
+      res.status(403).send('Invalid validation key');
+      return;
     }
+    console.log('Received form data:', formData);
+    const message = JSON.stringify(formData);
+
+    await bot.sendMessage(chatId, message);
+
+    console.log('Message sent successfully');
+    res.send('Data received and sent successfully');
   } catch (error) {
-    console.error(`Error reading file: ${error.message}`);
+    console.error('Error handling request:', error);
+    res.status(500).send('Error handling request');
   }
-};
+});
 
-function run() {
-  setInterval(readAndSendMessages, 30000);
-};
+app.use(router);
 
-app.use(bodyParser.json()) // for parsing application/json
-app.use(
-	bodyParser.urlencoded({
-		extended: true,
-	})
-) // for parsing application/x-www-form-urlencoded
-
-//This is the route the API will call
-app.get("/", function(req, res) {
-	run();
-})
-
-// Finally, start our server
-const port = process.env.PORT || 3000
-app.listen(port, function() {
-	console.log("Telegram app listening on port 3000!")
-})
+const port = process.env.PORT || 3300;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
