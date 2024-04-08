2d549bdad56ed93815c771c131374accfe3d5efc
mromadisiregar <mromadisiregar@gmail.com>
2024-04-08 22:47:54 +0700

    test telegram 2

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const TelegramBot = require('node-telegram-bot-api');
 
-const token = '${{shared.TOKEN}}';
+const token = ${{shared.TOKEN}};
 const bot = new TelegramBot(token, { polling: true });
-const chatId = '${{shared.CHATID}}';
+const chatId = ${{shared.CHATID}};
 
 const app = express();
 const router = express.Router();
@@ -15,7 +15,7 @@ router.post('/bot_notifikasi_cpanel', async (req, res) => {
   try {
     const formData = req.body;
     const receivedKey = formData.validation_key;
-    const validationKey = '${{shared.VALIDATIONKEY}}';
+    const validationKey = ${{shared.VALIDATIONKEY}};
     if (receivedKey !== validationKey) {
       res.status(403).send('Invalid validation key');
       return;
